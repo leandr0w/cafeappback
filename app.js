832f919fde5dc44b1ee20710ce6464c7be4c1593
leandro wasinger <leowasingerz@gmail.com>
2023-03-16 22:00:54 -0300

    formatting code

--- app.js
+++ app.js
@@ -1,51 +1,58 @@
-//rrequerimos express que es una libreria
-const express = require('express')
+//requerimos express que es una libreria
+const express = require('express');
 //iniciamos la app de expres en la variable app
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const findAllProducts = (req, res) => {   
-    res.json({
-        message: "hello from the get route"
-    })
-}
+const findAllProducts = (req, res) => {
+  res.json({
+    message: 'hello from the get route',
+  });
+};
 
-//creamos una ruta de mi servidor
-app.get('/api/v1/products', findAllProducts)
+const findOneProduct = (req, res) => {
+  console.log(req.params.id);
+  res.json({
+    message: 'hello from the getoneproduct route',
+  });
+};
 
 const createProduct = (req, res) => {
-
-    console.log(req.body)
-    res.json({
-        message: 'hell from the post route'
-    })
-}
-
-app.post('/api/v1/products', createProduct)
-
-const updateProduct =  (req, res) => {
-    console.log(req.params)
-    console.log('me ejecute')
-    res.json({
-        message: 'hell from the patch route'
-    })
-}
-
-app.patch('/api/v1/products/:id', updateProduct)
-
+  console.log(req.body);
+  res.json({
+    message: 'hell from the post route',
+  });
+};
 const deleteProduct = (req, res) => {
-    console.log('me ejecute')
-    res.json({
-        message: 'hell from the delete route'
-    })
-}
+  console.log('me ejecute');
+  res.json({
+    message: 'hell from the delete route',
+  });
+};
 
-app.delete('/api/v1/products/:id', deleteProduct)
+const updateProduct = (req, res) => {
+  console.log(req.params);
+  console.log('me ejecute');
+  res.json({
+    message: 'hell from the patch route',
+  });
+};
 
-const port = 3000
+app
+  .route('/api/v1/products')
+  .get(findAllProducts)
+  .post(createProduct);
 
-//colocar la app a escuchar el servidor por el puerto 
+app
+  .route('/api/v1/products/:id')
+  .get(findOneProduct)
+  .patch(updateProduct)
+  .delete(deleteProduct);
+
+const port = 3000;
+
+//colocar la app a escuchar el servidor por el puerto
 app.listen(port, () => {
-    console.log(`app running on port ${port}`)
-})
+  console.log(`app running on port ${port}`);
+});
